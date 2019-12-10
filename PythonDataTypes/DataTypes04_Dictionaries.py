9d8d6de53b748a00b97ad087ebd7fc0643205d7c
Ankit Mahendru <ankitmalikpkl@gmail.com>
2019-12-10 20:29:14 +0530

    Update #5

--- PythonDataTypes/DataTypes04_Dictionaries.py
+++ PythonDataTypes/DataTypes04_Dictionaries.py
@@ -2,9 +2,22 @@
 # Dictionaries in Python
 #
 
+my_dict = {'key1': 'value1', 'key2' : 'value2'}
 
 print(my_dict['key1'])
 
 dict = {'k1' : 123 , 'k2' : 'String', 'k3' : [1,2,3], 'k4' : {'k41' : 100} }
 
 print(dict['k4']['k41'])
+
+mylist = dict['k3']
+
+print(mylist)
+
+letter = mylist[2]
+
+print(letter)
+
+# OR
+
+print(dict['k3'][2])
