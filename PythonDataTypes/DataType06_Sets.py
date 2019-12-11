373ead586ee54d01963d014c427da46433e5f2bb
Ankit Mahendru <ankitmalikpkl@gmail.com>
2019-12-11 17:13:43 +0530

    Update #9 Sets

---
+++ PythonDataTypes/DataType06_Sets.py
@@ -0,0 +1,25 @@
+#
+# SETS
+#
+
+myset = set()
+
+print(myset)
+
+myset.add(1)
+
+myset.add(2)
+
+print(myset)
+
+# You cannot add one value twice
+
+myset.add(2)
+
+print(myset)
+
+mylist = [1,1,1,1,2,2,2,4,3,3,3,3,]
+
+print(
+    set(mylist)
+)
